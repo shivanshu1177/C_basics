df1ba53e0d166426bc83b6117df3b560721a1a19
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2021-12-16 19:05:12 +0530

    main

---
+++ AreaofCircle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float r, area;
+
+    printf("Enter Radius here:");
+
+    scanf("%f", &r);
+
+    area = r * r * 3.14;
+
+    printf("%f", area);
+
+}
---
+++ AreaofRect.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int length, breadth, Area;
+
+    scanf("%i" , &length);
+    scanf("%i" , &breadth);
+
+    Area = length * breadth ;
+
+    printf("%i" , Area);
+}
