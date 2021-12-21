130a79424712941a20c5097fa45688be0f64119e
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2021-12-21 18:37:29 +0530

    learning

---
+++ starpattern2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=5;
+
+    for (int i = 0; i < n; i++)
+    {
+        
+        for (int j = 0; j < n; j++)
+        {
+            if(i+j<=n-2)
+            {
+                printf(" ");
+
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+                
+        printf("\n");
+        
+    }
+
+    return 0;
+}
