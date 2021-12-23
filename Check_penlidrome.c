335dd325639e1bcbccd4c606d0608dd27a957832
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2021-12-24 01:39:18 +0530

    assignment

---
+++ Check_penlidrome.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, reminder, rev;
+
+    printf("Enter a Number: ");
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        reminder = n % 10 ;
+        rev = rev*10 + reminder;
+        n /= 10;
+
+    }
+    if (n = rev)
+    {
+        printf("Penlidrome");
+    }
+    else
+    {
+        printf("Not Pelnidrome");
+    }
+    
+    
+    return 0;
+}
