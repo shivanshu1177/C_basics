62a4bda87dc08c3c493d1181f3b25c8232f0ddf9
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2021-12-27 20:14:48 +0530

    fn assignment

---
+++ swapTwonum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+int main()
+{
+
+    int x, y;
+
+    printf("Enter x:");
+    scanf("%i", &x);
+
+    printf("Enter y:");
+    scanf("%i", &y);
+
+    swap(&x, &y);
+    printf("After swapping: x = %i, y = %i", x, y);
+
+
+    return 0;
+}
